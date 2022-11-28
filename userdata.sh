85659bca9f217d7b94f2d985906e506c16cddee9
Ilyasse <ilyassesalama7@gmail.com>
2022-11-28 19:47:23 +0100

    feat: Banner, user existence, more handlers

--- userdata.sh
+++ userdata.sh
@@ -1,18 +1,85 @@
 #!/bin/bash
 
+# colors
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+# variables
+IS_FIRST_TIME=true
+USER_EXISTS=false
+USER_NAME="Ilyasse Salama"
+
+# show banner
+init_banner() {
+	echo -e	"${RED}
+             ░░███╗░░██████╗░██████╗░███████╗
+             ░████║░░╚════██╗╚════██╗╚════██║
+             ██╔██║░░░█████╔╝░█████╔╝░░░░██╔╝
+             ╚═╝██║░░░╚═══██╗░╚═══██╗░░░██╔╝░
+             ███████╗██████╔╝██████╔╝░░██╔╝░░
+             ╚══════╝╚═════╝░╚═════╝░░░╚═╝░░░
+
+      ${NO_COLOR} ███████╗██╗███╗░░██╗██████╗░███████╗██████╗░
+       ██╔════╝██║████╗░██║██╔══██╗██╔════╝██╔══██╗
+       █████╗░░██║██╔██╗██║██║░░██║█████╗░░██████╔╝
+       ██╔══╝░░██║██║╚████║██║░░██║██╔══╝░░██╔══██╗
+       ██║░░░░░██║██║░╚███║██████╔╝███████╗██║░░██║
+       ╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝"
+
+	   if [ "$IS_FIRST_TIME" = true ]; then
+		   echo -e	"\n  Created as a fun project to find information about students"
+	   fi
+	   echo -e	"\n\n"
+}
+
 init_program() {
 	exec 2> /dev/null
 	clear
+	init_banner
+	IS_FIRST_TIME=false
 	echo "Enter the user login:"
 	read -a usr
-	username=${usr}
+	USER_NAME=${usr}
+
+	check_if_user_exists
+}
+
+check_if_user_exists() {
+	USER_INFO=$(ldapsearch uid=$USER_NAME | grep givenName)
+	USER_INFO=$(awk '{print $1}' <<< $USER_INFO | tr -d '[:]')
+
+	if [ "$USER_INFO" = "givenName" ]; then
+		prompt_user_menu
+	else
+		prompt_user_not_found
+	fi
+}
+
+get_user_phone(){
 	clear
-	echo -e "--- Choose the information you need ---\n1. Get phone number.\n2. Get full name.\n\nSelect:"
+	init_banner
+	echo "--- The phone number of $USER_NAME ---"
+	ldapsearch uid=$USER_NAME | grep mobile: | awk '{print $2}'
+
+	prompt_menu
+}
+
+get_user_full_name(){
+	clear
+	init_banner
+	echo "--- The full name of $USER_NAME ---"
+	ldapsearch uid=$USER_NAME | grep cn: | awk '{print $2 " " $3 " " $4}'
+
+	prompt_menu
+}
+
+prompt_user_menu(){
+	clear
+	init_banner
+	echo -e "Choose the information you need:\n1. Get phone number.\n2. Get full name.\n\nSelect:"
 	read -a var
 	chosen_info=${var}
 
-	check_if_user_exists
-	
 	if [ $chosen_info -eq 1 ]; then
 		get_user_phone
 	elif [ $chosen_info -eq 2 ]; then
@@ -21,29 +88,18 @@ init_program() {
 		echo "Wrong input."
 	fi
 
-	promt_menu
+	prompt_end_menu
 }
 
-check_if_user_exists() {
-	if [ldapsearch uid=$username | grep givenName | wc -l   ]; then
-		echo "User does not exist."
-		exit 1
-	fi
-}
-
-get_user_phone(){
+prompt_user_not_found(){
 	clear
-	echo "--- The phone number of $username ---"
-	ldapsearch uid=$username | grep mobile: | awk '{print $2}'
-}
-cccc
-get_user_full_name(){
-	clear
-	echo "--- The full name of $username ---"
-	ldapsearch uid=$username | grep cn: | awk '{print $2 " " $3 " " $4}'
+	init_banner
+	echo -e "\n"
+	echo "Student not found."
+	prompt_end_menu
 }
 
-promt_menu(){
+prompt_end_menu(){
 	echo -e "\n\nWhat do you want to do?\n1. Go back to the main menu.\n2. Exit.\n\nSelect:"
 
 	read -a reset_option
